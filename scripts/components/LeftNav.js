fad6c767491113ab888fb5dafef5a4b129ab7967
HugoDF <hdifrancesco@gmail.com>
2016-07-31 22:18:08 +0100

    Update 2016-07-31T21:17:59.382Z

--- scripts/components/LeftNav.js
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-
-export default class LeftNav extends React.Component {
-  render() {
-    return <div className="LeftNav">LEFT NAV</div>;
-  }
-}
--- scripts/components/RightNav.js
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-
-export default class RightNav extends React.Component {
-  render() {
-    return <div className="RightNav">RIGHT NAV</div>;
-  }
-}
